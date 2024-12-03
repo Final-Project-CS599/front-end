85337685386b329884772a8021fd7706cd97d31a
IsraaGamalAdel <139555581+IsraaGamalAdel@users.noreply.github.com>
2024-12-03 06:59:27 +0200

    Revert "Revert "Admin""

---
+++ src/components/pages/admin/Student/Student.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+import { Helmet } from 'react-helmet';
+
+export default function Student() {
+return <>
+    <Helmet>
+        <meta name='description' content='' />
+        <title>Student</title>
+    </Helmet>
+    <div className=' container'>
+        <div className='row'>
+                <div className=' col-md-6 p-2'>
+                    <div className=''>
+                        <h2>Student</h2>
+                    </div>
+                </div>
+                <div  className=' col-md-6 p-3'>
+                    <div className='d-flex justify-content-end'>
+                        <Link className='btn buttoncolor shadow'  to={'/registerStudent'}>Add New Student</Link>
+                    </div>
+                </div>
+        </div>
+    </div>
+    
+</>
+}
