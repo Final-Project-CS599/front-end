0518a78381094513e5d467fc75956fdb96b265fa
EsraaIsmaiel1 <esraa.esmaiel@gmail.com>
2025-02-06 07:30:31 +0200

    edited payment screen

--- src/pages/admin/Payment/Payment.jsx
+++ src/pages/admin/Payment/Payment.jsx
@@ -6,27 +6,31 @@ function PaymentMessages() {
   const { data } = useGetPayments();
 
   return (
-    <div className="container mt-5">
-      <h2 className="text-center mb-4">Payment Messages</h2>
-      <div className="list-group">
-        {data?.data?.map((msg) => (
-          <Link key={msg.id} to={`/admin/payment/${msg.id}`} className="text-decoration-none">
-            <div className="list-group-item mb-3 shadow-sm">
-              <div className="d-flex align-items-center">
-                <img
-                  src={`http://localhost:3000/${msg.img}`}
-                  alt="Payment Proof"
-                  className="img-fluid rounded mr-3"
-                  style={{ maxWidth: '150px' }}
-                />
-                <div>
-                  <h6 className="mb-1">{msg.email}</h6>
+    <div className="container ">
+      <h2 className=" mb-4">Payment Messages</h2>
+      {data?.data?.length === 0 ? (
+        <p className="text-center">No messages found.</p>
+      ) : (
+        <div className="list-group">
+          {data?.data?.map((msg) => (
+            <Link key={msg.id} to={`/admin/payment/${msg.id}`} className="text-decoration-none">
+              <div className="list-group-item mb-3 shadow-sm">
+                <div className="d-flex align-items-center justify-content-around">
+                  <img
+                    src={`http://localhost:3000/${msg.img}`}
+                    alt="Payment Proof"
+                    className="img-fluid rounded mr-3"
+                    style={{ maxWidth: '150px' }}
+                  />
+                  <h6 className="mb-1">
+                    Initiation Date :{new Date(msg.initiation_date).toLocaleDateString()}
+                  </h6>
                 </div>
               </div>
-            </div>
-          </Link>
-        ))}
-      </div>
+            </Link>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
